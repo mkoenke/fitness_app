0615784ce1366aca26e57e475f8d0a89de8b0b0b
jomariepolanco <jomariepolanco@gmail.com>
2020-11-10 17:33:00 -0500

    parsed date and time in appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,4 +5,12 @@ class Appointment < ApplicationRecord
     def start_time
         self.date ##Where 'start' is a attribute of type 'Date' accessible through MyModel's relationship
     end
+
+    def parsed_date
+        self.date.strftime("%m/%d/%Y")
+    end
+
+    def parsed_time
+        self.time.strftime("%I:%M %p")
+    end
 end
