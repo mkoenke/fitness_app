49d17de30f55889e83c716d99e9aa7439b4fb9b8
jomariepolanco <jomariepolanco@gmail.com>
2020-11-11 14:32:16 -0500

    took authorization off root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     # before_action :logged_in_user
-    before_action :authorization
+    skip_before_action :authorization, only: 'welcome#index'
 
   def logged_in_user 
     @current_user = User.find_by(id: session[:user_id])
