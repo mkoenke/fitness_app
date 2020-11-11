52b90a8b980aa08ef3989a6dd78b81202d575175
mkoenke <mkoenke@sas.upenn.edu>
2020-11-11 10:34:28 -0500

    password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
     has_many :workouts
     has_many :workouts, through: :appointments
     has_many :exercises, through: :stats
+    
+    validates :username, :password, presence: true
+    validates :password, confirmation: { case_sensitive: true}
 
     has_secure_password
  
