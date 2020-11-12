175f64d9723ef94182f5dcd3f743e4bce770fccd
jomariepolanco <jomariepolanco@gmail.com>
2020-11-12 14:54:37 -0500

    all workout appointment create done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/appointments/new_all_workouts', to: 'appointments#new_all_workouts', as: 'new_all_workouts'
+  post '/appointments', to: 'appointments#create_all_workouts', as: 'create_all_workouts'
   resources :appointments
   resources :stats
   resources :users
   resources :exercises
   resources :workouts
-  resources :joins
+  # resources :exercise_workouts
 
   delete '/sessions/logout', to: 'sessions#logout', as: 'logout'
   get '/sessions/new', to: 'sessions#new', as: 'new_login'
